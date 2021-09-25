c8c41315c8e89db67cf8e2c35c5241c80d217aa2
atingupta2005@gmail.com <Atin Gupta>
2021-09-25 09:41:36 +0000

    My commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+### RDS Configuration File
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 5
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "mypassword45852"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = "true"
+}
