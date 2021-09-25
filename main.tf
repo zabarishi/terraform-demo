2c71930e548875ad2d6215c08110681c6494c3ec
atingupta2005@gmail.com <Atin Gupta>
2021-09-25 09:45:03 +0000

    changed size to 4

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage    = 5
+  allocated_storage    = 4
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
