98c93f3cb58829269f8377baeb12795b99e9ecf0
atingupta2005@gmail.com <Atin Gupta>
2021-09-25 10:02:59 +0000

    -

--- main.tf
+++ main.tf
@@ -8,5 +8,5 @@ provider "aws" {
 # but has no significance outside that module's scope.
 resource "aws_instance" "myec2" {
   ami           = "ami-5b41123e"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 }
